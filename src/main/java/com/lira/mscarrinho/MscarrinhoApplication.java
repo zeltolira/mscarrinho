46096db2519c1e79443a428f5f3d88c9ffc59a66
zeltolira <zelto16@hotmail.com>
2025-06-27 06:06:45 -0300

    inicia projeto

---
+++ src/main/java/com/lira/mscarrinho/MscarrinhoApplication.java
@@ -0,0 +1,13 @@
+package com.lira.mscarrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscarrinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lira/mscarrinho/MscarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lira.mscarrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
