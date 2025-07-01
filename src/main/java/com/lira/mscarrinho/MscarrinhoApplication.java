52b852f0f5304116698f86475b2f08fc8c2c7fe9
zeltolira <zelto16@hotmail.com>
2025-07-01 06:41:08 -0300

    atualiza pom

--- src/main/java/com/lira/mscarrinho/MscarrinhoApplication.java
+++ src/main/java/com/lira/mscarrinho/MscarrinhoApplication.java
@@ -1,10 +1,23 @@
 package com.lira.mscarrinho;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "carrinho")
+@Log4j2
 public class MscarrinhoApplication {
+
+	@GetMapping
+	public String GetHomeTest(){
+		log.info("Obtendo status do microsservice de carrinho");
+		return "Minha Loja Virtual.com - Microsservice!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscarrinhoApplication.class, args);
